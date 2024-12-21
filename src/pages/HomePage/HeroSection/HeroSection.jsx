b67ec53a39fc937b9cbcdbc1974cb920b2231a54
Kazi Solayman <kazisolayman123@gmail.com>
2024-12-22 02:08:49 +0600

    worked ont he hero section and put the video bg

--- src/pages/HomePage/HeroSection/HeroSection.jsx
+++ src/pages/HomePage/HeroSection/HeroSection.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import './HeroSection.css';
 
 const HeroSection = () => {
     return (
-        <div className='bg-gray-500 py-20'>
-            <div className="app-container">
-                <h1 className='text-white text-8xl  font-semibold '> Views beyond <br /> imagination</h1>
-                <h4 className='text-3xl my-7 text-white'>Golden Willows, a Hiranandani Project in Panvel</h4>
-                <p className='text-gray-300 mb-3'>8 Buildings - Fortune City Project</p>
-                <p className='text-white lg:w-5/12'>Indulge in opulence at the heart of Panvel with Golden Willows— seamless connectivity, world-class amenities, and stunning views.</p>
-                <button className='flex items-center justify-center w-80 bg-white p-2 rounded-md mt-5'>Contact</button>
+        <div className='relative h-screen'>
+            {/* Background Video */}
+            <video
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                src="/images/hero-video.mp4"
+                autoPlay
+                loop
+                muted
+            ></video>
+
+            {/* Overlay Content */}
+            <div className='bg-black bg-opacity-40  relative z-10 flex flex-col  justify-center h-full  '>
+                <div className="app-container main-hero">
+                    <h1 className='text-white text-8xl font-semibold'><span>Views beyond</span> <br /> imagination</h1>
+                    <h4 className='text-3xl my-8 text-white'>Golden Willows, a Hiranandani Project in Panvel</h4>
+                    <p className='text-gray-300 mb-3'>8 Buildings - Fortune City Project</p>
+                    <p className='text-white lg:w-5/12 '>Indulge in opulence at the heart of Panvel with Golden Willows— seamless connectivity, world-class amenities, and stunning views.</p>
+                    <button className='w-[24rem] bg-white p-2 rounded-md mt-8'>Contact</button>
+                </div>
             </div>
         </div>
     );
