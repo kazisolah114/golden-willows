75a243bc8c632446c353f26b815ea2d4c804af53
Kazi Solayman <kazisolayman123@gmail.com>
2024-12-22 01:37:24 +0600

    worked ont eh footer section

--- src/pages/HomePage/GallerySection/GallerySection.jsx
+++ src/pages/HomePage/GallerySection/GallerySection.jsx
@@ -22,41 +22,43 @@ const GallerySection = () => {
 
     return (
         <section className=" py-20">
-            <h4 className="text-lg mb-16">Gallery</h4>
-            <Swiper
-                onBeforeInit={(swiper) => {
-                    swiperRef.current = swiper;
-                }}
-                spaceBetween={20}
-                slidesPerView={1}
-                // navigation
-                // pagination={{ clickable: true }}
-                // modules={[Navigation, Pagination]}
-                autoplay={{
-                    delay: 1000,
+            <div className="app-container">
+                <h4 className="text-lg mb-16">Gallery</h4>
+                <Swiper
+                    onBeforeInit={(swiper) => {
+                        swiperRef.current = swiper;
+                    }}
+                    spaceBetween={20}
+                    slidesPerView={1}
+                    // navigation
+                    // pagination={{ clickable: true }}
+                    // modules={[Navigation, Pagination]}
+                    autoplay={{
+                        delay: 1000,
 
-                }}
-                loop={true}
-                className="pb-10 app-container"
-            >
-                {slides.map((slide, index) => (
-                    <SwiperSlide key={index}>
-                        <div className="bg-gray-800 rounded-lg shadow-lg flex flex-col items-center justify-center relative">
-                            <Image src={slide} alt="slide" width={500} height={500} quality={100} priority className='w-full h-[44rem] rounded-lg object-fill' />
-                            <div className="about absolute top-10 left-0 w-full flex justify-between items-center px-5">
-                                <Image src="/images/hiranandani.png" alt="img" width={150} height={150} className='' />
-                                <Image src="/images/luxury-estate.png" alt="img" width={120} height={120} className='' />
+                    }}
+                    loop={true}
+                    className="pb-10"
+                >
+                    {slides.map((slide, index) => (
+                        <SwiperSlide key={index}>
+                            <div className="bg-gray-800 rounded-lg shadow-lg flex flex-col items-center justify-center relative">
+                                <Image src={slide} alt="slide" width={500} height={500} quality={100} priority className='w-full h-[44rem] rounded-lg object-fill' />
+                                <div className="about absolute top-10 left-0 w-full flex justify-between items-center px-5">
+                                    <Image src="/images/hiranandani.png" alt="img" width={150} height={150} className='' />
+                                    <Image src="/images/luxury-estate.png" alt="img" width={120} height={120} className='' />
+                                </div>
+                                <div className='absolute top-[50%] left-0 w-full flex justify-between items-center px-5'>
+                                    <button onClick={() => swiperRef.current?.slidePrev()}
+                                        aria-label="Previous" className='w-20 h-20 flex items-center justify-center text-xl border-gray-200 border text-white rounded-full bg-transparent backdrop-blur-md '><HiArrowLeft /></button>
+                                    <button onClick={() => swiperRef.current?.slideNext()}
+                                        aria-label="Next" className='w-20 h-20 flex items-center justify-center text-xl border-gray-200 border text-white rounded-full bg-transparent backdrop-blur-md'><HiArrowRight /></button>
+                                </div>
                             </div>
-                            <div className='absolute top-[50%] left-0 w-full flex justify-between items-center px-5'>
-                                <button onClick={() => swiperRef.current?.slidePrev()}
-                                    aria-label="Previous" className='w-20 h-20 flex items-center justify-center text-xl border-gray-200 border text-white rounded-full bg-transparent backdrop-blur-md '><HiArrowLeft /></button>
-                                <button onClick={() => swiperRef.current?.slideNext()}
-                                    aria-label="Next" className='w-20 h-20 flex items-center justify-center text-xl border-gray-200 border text-white rounded-full bg-transparent backdrop-blur-md'><HiArrowRight /></button>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
             <Amenities />
         </section>
     );
