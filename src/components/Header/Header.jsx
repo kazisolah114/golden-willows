3d05da25dd13e507717ef5ec08875819954056d5
Kazi Solayman <kazisolayman123@gmail.com>
2024-12-22 18:24:08 +0600

    added the logo header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <header className='app-container flex items-center justify-between py-4'>
-            <div>LOGO</div>
+            <div className='border border-gray-400 rounded-lg w-40 flex items-center justify-center'>
+                <Link href="/" className='w-full text-center font-semibold p-2 flex items-center justify-center gap-2'> <div className=' bg-black w-[6px] h-[6px] rounded-full '></div>Golden Willows</Link>
+            </div>
             <Navbar />
             <div>
-                <button className='border border-gray-500 rounded-md px-5 py-2'>Contact</button>
+                <button className='border border-gray-400 rounded-md px-5 py-2'>Contact</button>
             </div>
         </header>
     );
